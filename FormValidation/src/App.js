8ff604c1e0a65d7497489162d185401ef331b1a2
Subsin Boriboon <132213400+SubsinBoriboon@users.noreply.github.com>
2023-08-20 20:38:13 +0700

    Add files via upload

---
+++ FormValidation/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import FormComponent from './Component/formComponent';
+function App() {
+  return (
+    <div className="App">
+      <FormComponent />
+    </div>
+  );
+}
+
+export default App;
